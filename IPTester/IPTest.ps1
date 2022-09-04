b74b389fab521d22f95c5daffaac4c037e7db9dc
Onur Yılmaz <112782736+onuryilmazblog@users.noreply.github.com>
2022-09-04 10:35:43 +0300

    IP Test Aracı

---
+++ IPTester/IPTest.ps1
@@ -0,0 +1,34 @@
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+#$scriptPath = "C:\Install\IpTester"
+$CurrentDate = $(Get-Date -UFormat "%d.%m.%Y-%H.%M.%S")
+$ComputerName = $env:COMPUTERNAME
+
+## Get Config Veriable
+$Config = "$scriptPath\Config.xml"
+if (Test-Path $Config -ErrorAction SilentlyContinue) {
+    $Xml = [xml](Get-Content -Path $Config -ErrorAction SilentlyContinue)
+    [string]$LogFolderPath = $Xml.Configuration.Option | Select-Object -ExpandProperty LogFolderPath -ErrorAction SilentlyContinue
+    $IPTestCount = $Xml.Configuration.Option | Select-Object -ExpandProperty IPTestCount -ErrorAction SilentlyContinue
+}
+
+## Create Folder
+if (!(Test-Path $LogFolderPath)) { New-Item -Path "$LogFolderPath" -ItemType Directory -Force -ErrorAction Continue | Out-Null }
+
+## Get Check Computer List
+$ComputerList = Get-Content -Path "$scriptPath\ComputerIPList.txt"
+
+Foreach ($Computer in $ComputerList) {
+    $LogFileName = "$LogFolderPath\$ComputerName" + "_$Computer.log"
+    Start -FilePath "cmd" -ArgumentList "/c echo ================================================== >> $LogFileName" -NoNewWindow
+    Start-Sleep 3
+    Start -FilePath "cmd" -ArgumentList "/c echo $CurrentDate tarihinde baslatildi. >> $LogFileName" -NoNewWindow
+    Start-Sleep 3
+    Start -FilePath "cmd" -ArgumentList "/c echo ================================================== >> $LogFileName" -NoNewWindow
+    Start-Sleep 3
+}
+
+
+Foreach ($Computer in $ComputerList) {
+    $LogFileName = "$LogFolderPath\$ComputerName" + "_$Computer.log"
+    Start -FilePath "cmd" -ArgumentList "/c ping $Computer -n $IPTestCount >> $LogFileName" -NoNewWindow
+}
